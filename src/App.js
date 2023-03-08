616e71ce3922cdcc53b2934f9bad7c2321b6d6ea
visualdenniss <visualdennis@gmail.com>
2023-03-08 21:05:12 +0100

    import-export

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Gallery from './Gallery.js';
+import { Profile } from './Gallery.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+         <Profile />
+         <Gallery/>
     </div>
   );
 }
